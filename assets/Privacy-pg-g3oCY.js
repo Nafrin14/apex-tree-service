4aedcc7b3777c202413c8f0a6c37d8d92176c903
Nafrin14 <nafrinnafrin49@gmail.com>
2026-08-03 20:15:59 +0530

    Updates

--- assets/Privacy-pg-g3oCY.js
+++ assets/Privacy-pg-g3oCY.js
@@ -1,4 +1,4 @@
-import{t as e}from"./index-geKEHlGa.js";var t=e();function n(){return(0,t.jsx)(`section`,{className:`\r
+import{t as e}from"./index-B_zNtSgs.js";var t=e();function n(){return(0,t.jsx)(`section`,{className:`\r
         min-h-screen\r
         bg-[#F5EFE6]\r
         py-32\r
