bff91a454d949856153b5e0fdd11c4949bdff043
Nafrin14 <nafrinnafrin49@gmail.com>
2026-08-03 20:07:38 +0530

    Updates

--- assets/Terms-CxDkX7QI.js
+++ assets/Terms-CxDkX7QI.js
@@ -1,4 +1,4 @@
-import{t as e}from"./index-qLoUjErl.js";var t=e();function n(){return(0,t.jsx)(`section`,{className:`\r
+import{t as e}from"./index-DXIqYjib.js";var t=e();function n(){return(0,t.jsx)(`section`,{className:`\r
         min-h-screen\r
         bg-[#F5EFE6]\r
         py-32\r
